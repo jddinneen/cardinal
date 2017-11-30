3df50bbbbed91190f72abe5d67b85e0aa209620e
Jesse David Dinneen <jesse.dinneen@mail.mcgill.ca>
2017-11-30 20:07:01 +1300

    updated contact info

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ __copyright__ = "Copyright 2015, JDD"
 __license__ = "GPL"
 __version__ = "0.1"
 __maintainer__ = "Jesse David Dinneen"
-__email__ = "jesse.dinneen@mail.mcgill.ca"
+__email__ = "jesse.dinneen@vuw.ac.nz"
 __status__ = "Beta"
 
 
